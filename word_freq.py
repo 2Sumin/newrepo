4d4cf88a8654ce9feb533750ba44639551f32f71
2Sumin <lsmlsm426@knu.ac.kr>
2022-04-10 00:47:09 -0700

    ho05_4

--- word_freq.py
+++ word_freq.py
@@ -19,7 +19,7 @@ while True:
 		x.lower()
 		dic[x]=dic.get(x,0)+1
 
-dicSorted=sorted(d.items(), key=lambda y: y[1], reverse=True)
+dicSorted=sorted(dic.items(), key=lambda y: y[1], reverse=True)
 
 for i,j in dic.items():
 	print('{:10} {:>6}'.format(i,j))
