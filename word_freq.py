3e75dc135096bcd921c464cbf65276f252dafbfe
2Sumin <lsmlsm426@knu.ac.kr>
2022-04-14 01:44:16 -0700

    complete

--- word_freq.py
+++ word_freq.py
@@ -2,14 +2,13 @@
 import sys
 
 #1. open textfile
-#textfile=open('textfile.txt','r')
-#textfile=open(sys.argv[0],'r')
 
 filename=sys.argv[1]
 textfile=open(filename,'r')
+
 dic={}
 
-#2. read file line-by-line
+#2. read file 
 lines=textfile.readlines()
 
 for i in range (len(lines)) :
@@ -20,32 +19,22 @@ for i in range (len(lines)) :
 	for x in word:
 		x.lower()
 		dic[x]=dic.get(x,0)+1
-		#dicSorted=sorted(dic.dicsorted())
+		#another method to insert in dic	
 		#if x in dic:
 			#dic[x]+=1
 		#else:
 			#dic[x]=1
-#dicSorted=sorted(dic.items(), key=lambda y: y[1], reverse=True)
 
-dicSorted = sorted(dic.items(), 
-                              reverse=True, 
-                              key=lambda item: item[1])
+#4. Sort dictionary in descending order
+dicSorted = sorted(dic.items(), reverse=True,key=lambda item: item[1])
 
+#5. print Sorted dictionary
 cnt=0
 final_num=int(sys.argv[2])
 for key, value in dicSorted :
-	cnt+=1
 	if cnt == final_num:
 		break
-	#print(key, value)
-	
+	cnt+=1	
 	print('{:10} {:>6}'.format(key,value))
-#for i,j in dicSorted.items():
-	#print('{:10} {:>6}'.format(i,j))
-	#print(i,j)
-#print(dicSorted)	
-
-#for key, value in dicSorted.items():
-	#print(key, value)
 
 
